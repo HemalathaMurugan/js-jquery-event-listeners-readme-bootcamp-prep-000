2ee4b654c9640a00fff65e11a884936e8feb2ba3
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-28 06:42:58 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt(){
 }
 function submitIt(){
   $(document).on("submit",function(){
-    alert("Your fo")
+    alert("Your form is going to be submitted now.");
   })
 }
 $(document).ready(function(){
